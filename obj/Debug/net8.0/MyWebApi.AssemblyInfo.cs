dd48dbd03ff77b1a52235d13754366e0e5d60039
ilaydaakyuz <iakyuz894@gmail.com>
2024-09-05 15:20:31 +0300

    Update design improvements

--- obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e43a9f1590f96333ea2aaf2d5320d16258a35c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd83ffd552b78ce2d7b896b463750edaeeaf8ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
